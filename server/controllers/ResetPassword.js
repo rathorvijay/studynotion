8b0ffe8612c0d4d3f97f68ea9030d173875a2a74
rathorvijay <rathorvijay856@gmail.com>
2023-07-06 10:10:30 +0530

    version 1,3

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		const url = `${process.env.FRONTEND_URL}/update-password/${token}`;
 
 		await mailSender(
 			email,
